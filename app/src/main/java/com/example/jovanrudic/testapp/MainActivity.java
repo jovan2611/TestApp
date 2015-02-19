e0d9f46edd31258dd3f993c0e6deaadaab6097b3
jrudic <jovan.rudic@asseco-see.rs>
2015-02-19 11:46:03 +0100

    $ komit

--- app/src/main/java/com/example/jovanrudic/testapp/MainActivity.java
+++ app/src/main/java/com/example/jovanrudic/testapp/MainActivity.java
@@ -4,14 +4,30 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    TextView proba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        proba = (TextView) findViewById(R.id.tvTest);
+
+        Button but = (Button) findViewById(R.id.btnTest);
+
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                proba.setText("Testiranje");
+            }
+        });
     }
 
 
